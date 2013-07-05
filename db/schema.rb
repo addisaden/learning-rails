99f5df32318781134a4e1846e5f411c93e45a7b0
Adrian E <addisaden@gmail.com>
2013-07-05 11:03:49 +0200

    add migration to eliminate duplicated line items and count them into quantity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130705083700) do
+ActiveRecord::Schema.define(version: 20130705085230) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
